cbe69f5603626af5476a12f962d286a5958bc255
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2023-10-11 08:17:09 -0700

    fix saving number fields to fix #7

--- app/helpers/format-value.ts
+++ app/helpers/format-value.ts
@@ -1,6 +1,8 @@
-export function formatValue(value: string | string[] | null) {
+export function formatValue(value: string | string[] | number | null) {
   if (Array.isArray(value)) {
     return value.length ? value.join(", ") : null;
+  } else if (typeof value === "number") {
+    return value.toString();
   }
   return value ? value : null;
 }
